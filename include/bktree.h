52faf0f421401de4ab20cad87950b1f31622064a
Nikos Tsolkas <75496647+nicktso7@users.noreply.github.com>
2021-11-11 20:08:27 +0200

    Add files via upload

---
+++ include/bktree.h
@@ -0,0 +1,21 @@
+#include "string.h"
+#include "entry.h"
+#include "entry_list.h"
+#define LEN 30
+
+typedef struct treenode
+{
+    char* word;
+    node* next[LEN];
+}node;
+
+node* create_tree(char*);
+
+int find_distance(char*,char*);
+
+void add_node(node*,char*);
+
+entry_list* get_words(node*,char *,int);
+
+void delete_tree(node *);
+ 
